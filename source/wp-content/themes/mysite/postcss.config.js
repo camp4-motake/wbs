ed0ee0c64aa64a62a2ad8437596a014e19838a34
motake <munetoshi_otake@camp4.jp>
2024-01-06 22:15:23 +0900

    add cssnano

--- source/wp-content/themes/mysite/postcss.config.js
+++ source/wp-content/themes/mysite/postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = () => ({
-	plugins: {
-		'postcss-preset-env': { stage: 2, features: { 'nesting-rules': true } },
-		'postcss-sort-media-queries': {},
-	},
-});
+module.exports = (ctx) => {
+	const inProduction = ctx.mode === 'production';
+
+	return {
+		plugins: {
+			'postcss-preset-env': {
+				stage: 2,
+				features: { 'nesting-rules': true },
+			},
+			'postcss-sort-media-queries': {},
+			...(inProduction ? { cssnano: { preset: 'default' } } : null),
+		},
+	};
+};
