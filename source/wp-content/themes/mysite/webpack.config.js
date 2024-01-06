3cbbb65d9900042f20f23e2cbc143950a7785faf
motake <munetoshi_otake@camp4.jp>
2024-01-06 23:23:53 +0900

    refactoring

--- source/wp-content/themes/mysite/webpack.config.js
+++ source/wp-content/themes/mysite/webpack.config.js
@@ -3,39 +3,41 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 const svgFilter = (_source, sourcePath) => !/\.(svg)$/i.test(sourcePath);
+const config = defaultConfig;
 
-module.exports = {
-	...defaultConfig,
-	stats: 'minimal',
-	optimization: {
+config.stats = 'minimal';
+
+config.optimization.minimizer.push(
+	new ImageMinimizerPlugin({
 		minimizer: [
-			// @see https://github.com/webpack-contrib/image-minimizer-webpack-plugin
-			new ImageMinimizerPlugin({
-				minimizer: [
-					// { implementation: ImageMinimizerPlugin.sharpMinify, filter: svgFilter, options: { encodeOptions: {} }, },
-					{
-						implementation: ImageMinimizerPlugin.svgoMinify,
-						options: {
-							encodeOptions: {
-								multipass: true,
-								plugins: ['preset-default'],
-							},
-						},
+			{
+				implementation: ImageMinimizerPlugin.sharpMinify,
+				filter: svgFilter,
+				options: { encodeOptions: {} },
+			},
+			{
+				implementation: ImageMinimizerPlugin.svgoMinify,
+				options: {
+					encodeOptions: {
+						multipass: true,
+						plugins: ['preset-default'],
 					},
-				],
-				generator: [
-					{
-						type: 'asset',
-						implementation: ImageMinimizerPlugin.sharpGenerate,
-						filter: svgFilter,
-						options: { encodeOptions: { webp: { quality: 90 } } },
-					},
-				],
-			}),
+				},
+			},
 		],
-	},
-	plugins: [
-		...defaultConfig.plugins,
-		new CopyPlugin({ patterns: [{ from: 'src/images', to: 'images' }] }),
-	],
-};
+		generator: [
+			{
+				type: 'asset',
+				implementation: ImageMinimizerPlugin.sharpGenerate,
+				filter: svgFilter,
+				options: { encodeOptions: { webp: { quality: 90 } } },
+			},
+		],
+	})
+);
+
+config.plugins.push(
+	new CopyPlugin({ patterns: [{ from: 'src/images', to: 'images' }] })
+);
+
+module.exports = config;
