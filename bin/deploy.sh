c11a5f4d712ad70924a8299639eb24482202116b
motake <munetoshi_otake@camp4.jp>
2024-01-07 15:32:18 +0900

    add plugin task example

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,28 +1,34 @@
 #! /bin/bash
 
+WP_SITE_SLUG=mysite
+
 if [ -e .env ]; then
     source .env
 fi
 
 case "$1" in
   "production")
-    if [[ -n "$RSYNC_PRODUCTION_REMOTE_PATH" ]];
+    if [[ -n "$RSYNC_PRODUCTION_REMOTE_WP_PATH" ]];
       then
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/"  -e ssh ./source/wp-content/themes/${WP_THEME_NAME}/ ${RSYNC_PRODUCTION_REMOTE_PATH}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/themes/${WP_SITE_SLUG}/ ${RSYNC_PRODUCTION_REMOTE_WP_PATH}/wp-content/themes/${WP_SITE_SLUG}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/plugins/${WP_SITE_SLUG}/ ${RSYNC_PRODUCTION_REMOTE_WP_PATH}/wp-content/plugins/${WP_SITE_SLUG}
       else
         echo 'Error: No remote path' && exit 0
     fi
     ;;
   "test")
-    if [[ -n "$RSYNC_TEST_REMOTE_PATH" ]];
+    if [[ -n "$RSYNC_TEST_REMOTE_WP_PATH" ]];
       then
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" -e ssh ./source/wp-content/themes/${WP_THEME_NAME}/ ${RSYNC_TEST_REMOTE_PATH}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/themes/${WP_SITE_SLUG}/ ${RSYNC_TEST_REMOTE_WP_PATH}/wp-content/themes/${WP_SITE_SLUG}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/plugins/${WP_SITE_SLUG}/ ${RSYNC_TEST_REMOTE_WP_PATH}/wp-content/plugins/${WP_SITE_SLUG}
       else
         echo 'Error: No remote path' && exit 0
     fi
     ;;
   "zip")
-    mkdir -p .zip && (cd ./source/wp-content/themes/${WP_THEME_NAME}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" - .) > .zip/${WP_THEME_NAME}.zip
+	  npm run build
+    mkdir -p .zip/themes && (cd ./source/wp-content/themes/${WP_SITE_SLUG}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/themes/${WP_SITE_SLUG}.zip
+    mkdir -p .zip/plugins && (cd ./source/wp-content/plugins/${WP_SITE_SLUG}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/plugins/${WP_SITE_SLUG}.zip
     ;;
   *)
     echo "undefined"
