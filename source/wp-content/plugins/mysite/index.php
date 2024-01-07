c3eb3024176b3df2101fa4acd34e9c47c2a504d6
motake <munetoshi_otake@camp4.jp>
2024-01-07 14:02:16 +0900

    refactoring

--- source/wp-content/plugins/mysite/index.php
+++ source/wp-content/plugins/mysite/index.php
@@ -1,21 +1,22 @@
 <?php
+
 /**
  * Plugin Name:       mysite plugin
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
- * Requires PHP:      7.0
+ * Description:       Custom Site Plugin
+ * Requires at least: 6.4
+ * Requires PHP:      8.1
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            CAMP4 inc.
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       mysite
  *
- * @package           create-block
+ * @package           mysite
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-require_once 'inc/acf-json.php';
-require_once 'inc/blocks.php';
+require_once 'inc/acf/acf-json.php';
+require_once 'inc/blocks/register-blocks.php';
