a95558fe0b32d6e4d7b26ba4b19e171de85d6eaa
motake <munetoshi_otake@camp4.jp>
2024-01-09 13:38:57 +0900

    update tasks

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-WP_SITE_SLUG=mysite
+WP_SITE_PREFIX=mysite
 
 if [ -e .env ]; then
     source .env
@@ -10,26 +10,27 @@ npm run build
 
 case "$1" in
   "production")
-    if [[ -n "$RSYNC_PRODUCTION_REMOTE_WP_PATH" ]];
+    if [[ -n "$PRODUCTION_WP_ROOT_PATH" ]];
       then
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/themes/${WP_SITE_SLUG}/ ${RSYNC_PRODUCTION_REMOTE_WP_PATH}/wp-content/themes/${WP_SITE_SLUG}
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/plugins/${WP_SITE_SLUG}/ ${RSYNC_PRODUCTION_REMOTE_WP_PATH}/wp-content/plugins/${WP_SITE_SLUG}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/themes/${WP_SITE_PREFIX}/ ${PRODUCTION_WP_ROOT_PATH}/wp-content/themes/${WP_SITE_PREFIX}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${PRODUCTION_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
       else
         echo 'Error: No remote path' && exit 0
     fi
     ;;
   "test")
-    if [[ -n "$RSYNC_TEST_REMOTE_WP_PATH" ]];
+    if [[ -n "$TEST_WP_ROOT_PATH" ]];
       then
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/themes/${WP_SITE_SLUG}/ ${RSYNC_TEST_REMOTE_WP_PATH}/wp-content/themes/${WP_SITE_SLUG}
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/plugins/${WP_SITE_SLUG}/ ${RSYNC_TEST_REMOTE_WP_PATH}/wp-content/plugins/${WP_SITE_SLUG}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/themes/${WP_SITE_PREFIX}/ ${TEST_WP_ROOT_PATH}/wp-content/themes/${WP_SITE_PREFIX}
+        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${TEST_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
       else
         echo 'Error: No remote path' && exit 0
     fi
     ;;
   "zip")
-    mkdir -p .zip/themes && (cd ./source/wp-content/themes/${WP_SITE_SLUG}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/themes/${WP_SITE_SLUG}.zip
-    mkdir -p .zip/plugins && (cd ./source/wp-content/plugins/${WP_SITE_SLUG}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/plugins/${WP_SITE_SLUG}.zip
+		rm -rf .zip/
+    mkdir -p .zip/themes && (cd ./source/wp-content/themes/${WP_SITE_PREFIX}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/themes/${WP_SITE_PREFIX}.zip
+    mkdir -p .zip/plugins && (cd ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/plugins/${WP_SITE_PREFIX}-plugin.zip
     ;;
   *)
     echo "undefined"
