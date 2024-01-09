31d8b5acf255ecee757615cef7493718a0bcb3fa
motake <munetoshi_otake@camp4.jp>
2024-01-09 17:12:37 +0900

    add env port pattern

--- bs-config.js
+++ bs-config.js
@@ -4,8 +4,11 @@ const wpEnv = existsSync('./.wp-env.override.json')
 	? require('./.wp-env.override.json')
 	: require('./.wp-env.json');
 
+const { PROXY_URL, WP_ENV_PORT } = process.env;
+const wpPort = WP_ENV_PORT || wpEnv?.port || 8888;
+
 module.exports = {
-	files: workspaces.flatMap((path) => [
+	files: workspaces?.flatMap((path) => [
 		`${path}/**/*.{php,html,json}`,
 		`${path}/build`,
 		`!${path}/{vendor,node_modules}`,
@@ -14,6 +17,6 @@ module.exports = {
 	ghostMode: false,
 	notify: false,
 	open: false,
-	proxy: process.env.PROXY_URL || `http://localhost:${wpEnv?.port || 8888}`,
+	proxy: PROXY_URL || `http://localhost:${wpPort}`,
 	ui: false,
 };
