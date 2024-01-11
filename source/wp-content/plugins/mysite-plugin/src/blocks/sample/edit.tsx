bb3afb0e01db13ffd9e71ed88a65847366974875
motake <munetoshi_otake@camp4.jp>
2024-01-11 12:16:00 +0900

    adjustment

--- source/wp-content/plugins/mysite-plugin/src/blocks/sample/edit.tsx
+++ source/wp-content/plugins/mysite-plugin/src/blocks/sample/edit.tsx
@@ -24,10 +24,6 @@ import './editor.css';
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
  */
 export default function Edit() {
 	return (
