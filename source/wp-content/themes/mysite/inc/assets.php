95d307ff1f7d99cb2f88138f54ef2d750f07b98f
motake <munetoshi_otake@camp4.jp>
2024-01-19 12:21:21 +0900

    update example

--- source/wp-content/themes/mysite/inc/assets.php
+++ source/wp-content/themes/mysite/inc/assets.php
@@ -4,30 +4,31 @@ namespace Site\Theme\Assets;
 
 function enqueue_theme_assets() {
 
-	$asset_meta = include_once get_theme_file_path( 'build/index.asset.php' );
-	$version    = isset( $asset_meta['version'] ) ? $asset_meta['version'] : null;
+	$asset = include_once get_theme_file_path( 'build/main.asset.php' );
 
 	wp_enqueue_style(
 		'app-styles',
-		get_theme_file_uri( '/build/index.css' ),
+		get_theme_file_uri( 'build/main.css' ),
 		array(),
-		$version
+		$asset['version']
 	);
 
 	wp_enqueue_script(
 		'app-scripts',
-		get_theme_file_uri( 'build/index.js' ),
+		get_theme_file_uri( 'build/main.js' ),
 		array(),
-		$version
+		$asset['version']
 	);
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_assets' );
 
 
 function replace_module_script_tag( string $tag, string $handle, string $src ) {
+
 	if ( in_array( $handle, array( 'app-scripts' ), true ) ) {
 		return '<script type="module" src="' . esc_url( $src ) . '" defer></script>' . "\n";
 	}
+
 	return $tag;
 }
 add_filter( 'script_loader_tag', __NAMESPACE__ . '\replace_module_script_tag', 10, 3 );
