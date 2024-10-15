3a75e77a6d847774e46c9cbbf4e8faac4451729b
motake <munetoshi_otake@camp4.jp>
2024-10-15 13:16:58 +0900

    adjustment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,7 +13,7 @@ case "$1" in
     if [[ -n "$PRODUCTION_WP_ROOT_PATH" ]];
       then
         rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/themes/${WP_SITE_PREFIX}/ ${PRODUCTION_WP_ROOT_PATH}/wp-content/themes/${WP_SITE_PREFIX}
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${PRODUCTION_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
+        # rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo"  -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${PRODUCTION_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
       else
         echo 'Error: No remote path' && exit 0
     fi
@@ -22,7 +22,7 @@ case "$1" in
     if [[ -n "$TEST_WP_ROOT_PATH" ]];
       then
         rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/themes/${WP_SITE_PREFIX}/ ${TEST_WP_ROOT_PATH}/wp-content/themes/${WP_SITE_PREFIX}
-        rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${TEST_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
+        # rsync -av --delete --exclude ".DS_Store" --exclude ".gitkeep" --exclude="node_modules/" --exclude="vendor/" --exclude="*.tsbuildinfo" -e ssh ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ ${TEST_WP_ROOT_PATH}/wp-content/plugins/${WP_SITE_PREFIX}-plugin
       else
         echo 'Error: No remote path' && exit 0
     fi
@@ -30,7 +30,7 @@ case "$1" in
   "zip")
 		rm -rf .zip/
     mkdir -p .zip/themes && (cd ./source/wp-content/themes/${WP_SITE_PREFIX}/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/themes/${WP_SITE_PREFIX}.zip
-    mkdir -p .zip/plugins && (cd ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/plugins/${WP_SITE_PREFIX}-plugin.zip
+    # mkdir -p .zip/plugins && (cd ./source/wp-content/plugins/${WP_SITE_PREFIX}-plugin/ && zip -r -x "*.DS_Store" "*__MACOSX*" "*node_modules*" "*vendor*" "*.tsbuildinfo"  - .) > .zip/plugins/${WP_SITE_PREFIX}-plugin.zip
     ;;
   *)
     echo "undefined"
