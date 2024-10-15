b91a97115dbe97010b1ed8dd0093467c74c6329e
motake <munetoshi_otake@camp4.jp>
2024-10-15 13:43:46 +0900

    change wp_enqueue_script_module

--- source/wp-content/themes/mysite/inc/assets.php
+++ source/wp-content/themes/mysite/inc/assets.php
@@ -8,12 +8,9 @@
 
 namespace Site\Theme\Assets;
 
-/**
- * Main assets
- */
 function enqueue_theme_assets() {
 
-	$asset = include_once get_theme_file_path( 'build/main/index.asset.php' );
+	$asset = include get_theme_file_path( 'build/main/index.asset.php' );
 
 	wp_enqueue_style(
 		'theme-styles',
@@ -22,27 +19,12 @@ function enqueue_theme_assets() {
 		$asset['version']
 	);
 
-	wp_enqueue_script(
+	wp_enqueue_script_module(
 		'theme-scripts',
 		get_theme_file_uri( 'build/main/index.js' ),
-		array(),
+		$asset['dependencies'],
 		$asset['version'],
 		false
 	);
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_theme_assets', 100 );
-
-/**
- * replace module script tag
- */
-function replace_module_script_tag( string $tag, string $handle, string $src ) {
-
-	$targets = array( 'theme-scripts' );
-
-	if ( in_array( $handle, $targets, true ) ) {
-		return '<script type="module" src="' . esc_url( $src ) . '" defer></script>' . "\n"; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
-	}
-
-	return $tag;
-}
-add_filter( 'script_loader_tag', __NAMESPACE__ . '\\replace_module_script_tag', 10, 3 );
