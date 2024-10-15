fa21fc1f6d3ce1f7e021ea80e4b7dd0911ff30fb
motake <munetoshi_otake@camp4.jp>
2024-10-15 13:43:30 +0900

    add module build config

--- source/wp-content/themes/mysite/webpack.config.js
+++ source/wp-content/themes/mysite/webpack.config.js
@@ -4,94 +4,68 @@ const config = require( '@wordpress/scripts/config/webpack.config' );
 const imgFilter = ( _source, sourcePath ) => ! /\.(svg)$/i.test( sourcePath );
 
 /**
- * Stats
- * @see https://webpack.js.org/configuration/stats/
+ * webpack config
+ * @see https://webpack.js.org/configuration/
  */
-config.stats = 'minimal';
+for ( const cfg of config ) {
+	cfg.stats = 'minimal';
 
-/**
- * Cache
- * @see https://webpack.js.org/configuration/cache/
- */
-config.cache = {
-	type: 'filesystem',
-	buildDependencies: { config: [ __filename ] },
-};
-
-/**
- * Image minimize
- * @see https://github.com/webpack-contrib/image-minimizer-webpack-plugin
- */
-config.plugins.push(
-	new ImageMinimizerPlugin( {
-		test: /\.(svg)$/i,
-		minimizer: {
-			implementation: ImageMinimizerPlugin.svgoMinify,
-			options: {
-				encodeOptions: {
-					multipass: true,
-					plugins: [ 'preset-default' ],
-				},
-			},
-		},
-	} ),
-
-	new ImageMinimizerPlugin( {
-		minimizer: {
-			implementation: ImageMinimizerPlugin.sharpMinify,
-			options: {
-				encodeOptions: {
-					jpeg: { quality: 80 },
-					webp: { quality: 90, smartSubsample: true },
-					avif: { lossless: true },
-					png: { quality: 80 },
-				},
-			},
-		},
-		generator: [
-			{
-				type: 'asset',
-				implementation: ImageMinimizerPlugin.sharpGenerate,
-				filter: imgFilter,
+	cfg.plugins.push(
+		new ImageMinimizerPlugin( {
+			test: /\.(svg)$/i,
+			minimizer: {
+				implementation: ImageMinimizerPlugin.svgoMinify,
 				options: {
 					encodeOptions: {
-						webp: { quality: 90, smartSubsample: true },
+						multipass: true,
+						plugins: [ 'preset-default' ],
 					},
 				},
 			},
-		],
-	} ),
+		} ),
 
-	/**
-	 * Copy Files
-	 */
-	new CopyPlugin( {
-		patterns: [
-			{
-				from: 'src/images',
-				to: 'images',
-				globOptions: { ignore: [ '**/_inline/**' ] },
-				noErrorOnMissing: true,
+		new ImageMinimizerPlugin( {
+			minimizer: {
+				implementation: ImageMinimizerPlugin.sharpMinify,
+				options: {
+					encodeOptions: {
+						jpeg: { quality: 80 },
+						webp: { quality: 90, smartSubsample: true },
+						avif: { lossless: true },
+						png: { quality: 80 },
+					},
+				},
 			},
-		],
-	} )
-);
+			generator: [
+				{
+					type: 'asset',
+					implementation: ImageMinimizerPlugin.sharpGenerate,
+					filter: imgFilter,
+					options: {
+						encodeOptions: {
+							webp: { quality: 90, smartSubsample: true },
+						},
+					},
+				},
+			],
+		} ),
 
-config.module.rules.push( {
-	test: /\.(jpe?g|png|gif|svg)$/i,
-	type: 'asset',
-} );
+		new CopyPlugin( {
+			patterns: [
+				{
+					from: 'src/images',
+					to: 'images',
+					globOptions: { ignore: [ '**/_inline/**' ] },
+					noErrorOnMissing: true,
+				},
+			],
+		} )
+	);
 
-/**
- * Performance
- * @see https://webpack.js.org/configuration/performance/#performance-hints
- */
-config.performance = {
-	maxAssetSize: 768000,
-	maxEntrypointSize: 512000,
-	assetFilter( assetFilename ) {
-		return assetFilename.endsWith( '.js' );
-	},
-};
+	cfg.module.rules.push( {
+		test: /\.(jpe?g|png|gif|svg)$/i,
+		type: 'asset',
+	} );
+}
 
 module.exports = config;
