1b6a1002d61fc84563ac83fdb74257ebfc7fa5ee
motake <munetoshi_otake@camp4.jp>
2025-01-07 11:47:52 +0900

    housekeeping

--- bin/lint.mjs
+++ bin/lint.mjs
@@ -4,35 +4,19 @@ const packageJson = await import( '../package.json', {
 	with: { type: 'json' },
 } );
 const { workspaces } = packageJson.default;
+const composer = 'docker run --rm --volume $PWD:/app composer composer';
 
-const task = {
-	format: async () => {
-		await $`npx wp-scripts format . '!source'`;
-		await $`npm run format --workspaces --if-present`;
-		workspaces.map(
-			async ( ws ) =>
-				await $`docker run --rm --volume $PWD:/app composer composer format ${ ws }`
-		);
-	},
+$.verbose = true;
+process.env.FORCE_COLOR = '1';
 
-	lint: async () => {
-		await $`npm run lint --workspaces --if-present`;
-		workspaces.map(
-			async ( ws ) =>
-				await $`docker run --rm --volume $PWD:/app composer composer lint ${ ws }`
-		);
-	},
-};
-
-const run = () => {
-	$.verbose = true;
-	process.env.FORCE_COLOR = '1';
-
-	if ( process.argv.includes( '--format' ) ) {
-		task.format();
-	} else {
-		task.lint();
-	}
-};
-
-run();
+// format
+if ( process.argv.includes( '--format' ) ) {
+	await $`npx wp-scripts format . '!source'`;
+	await $`npm run format --workspaces --if-present`;
+	workspaces.forEach( async ( ws ) => await $`${ composer } format ${ ws }` );
+}
+// lint
+else {
+	await $`npm run lint --workspaces --if-present`;
+	workspaces.forEach( async ( ws ) => await $`${ composer } lint ${ ws }` );
+}
