e95219d3bad09ce0c6b4c7c8e5639625a6bc1772
motake <munetoshi_otake@camp4.jp>
2025-01-07 13:06:07 +0900

    fix commands

--- bin/lint.mjs
+++ bin/lint.mjs
@@ -4,7 +4,6 @@ const packageJson = await import( '../package.json', {
 	with: { type: 'json' },
 } );
 const { workspaces } = packageJson.default;
-const composer = 'docker run --rm --volume $PWD:/app composer composer';
 
 $.verbose = true;
 process.env.FORCE_COLOR = '1';
@@ -13,10 +12,14 @@ process.env.FORCE_COLOR = '1';
 if ( process.argv.includes( '--format' ) ) {
 	await $`npx wp-scripts format . '!source'`;
 	await $`npm run format --workspaces --if-present`;
-	workspaces.forEach( async ( ws ) => await $`${ composer } format ${ ws }` );
+	workspaces.forEach( async ( ws ) => {
+		await $`docker run --rm --volume $PWD:/app composer composer format ${ ws }`;
+	} );
 }
 // lint
 else {
 	await $`npm run lint --workspaces --if-present`;
-	workspaces.forEach( async ( ws ) => await $`${ composer } lint ${ ws }` );
+	workspaces.forEach( async ( ws ) => {
+		await $`docker run --rm --volume $PWD:/app composer composer lint ${ ws }`;
+	} );
 }
