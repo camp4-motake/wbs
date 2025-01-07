acb86db6146f96a0e11004149706a19883c33f25
motake <munetoshi_otake@camp4.jp>
2025-01-07 09:10:02 +0900

    adjustment

--- source/wp-content/themes/mysite/postcss.config.js
+++ source/wp-content/themes/mysite/postcss.config.js
@@ -1,1 +1,14 @@
-module.exports = require( '../../../../postcss.config.js' );
+// shared config
+module.exports = ( ctx ) => ( {
+	plugins: {
+		'postcss-easy-import': {},
+		'postcss-preset-env': {
+			stage: 2,
+			features: { 'nesting-rules': true },
+		},
+		'postcss-url': { filter: '**/_inline/*', url: 'inline' },
+		...( ctx.mode === 'production'
+			? { cssnano: { preset: 'default' } }
+			: {} ),
+	},
+} );
