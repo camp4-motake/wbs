a1b4188352fd4ba3e9645558c10c8e3adc7981bd
motake <munetoshi_otake@camp4.jp>
2025-01-07 19:39:03 +0900

    migrate pnpm

--- bin/wp-setup.mjs
+++ bin/wp-setup.mjs
@@ -3,6 +3,6 @@ import { $ } from 'zx';
 process.env.FORCE_COLOR = '1';
 $.verbose = true;
 
-await $`npx wp-env start`;
-await $`npx wp-env run cli ./env/setup.sh`;
-await $`npm run build`;
+await $`pnpm run wp-env start`;
+await $`pnpm run wp-env run cli ./env/setup.sh`;
+await $`pnpm run build`;
