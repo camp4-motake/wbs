27b32b5269aa53fb70944aa7fd46625a2c06ba84
motake <munetoshi_otake@camp4.jp>
2025-01-11 13:25:35 +0900

    add deps

--- bin/dev.mjs
+++ bin/dev.mjs
@@ -1,16 +1,13 @@
+import { findWorkspaces } from 'find-workspaces';
 import { $ } from 'zx';
 
-const packageJson = await import( '../package.json', {
-	with: { type: 'json' },
-} );
-const { workspaces } = packageJson.default;
+const workspaces = findWorkspaces()?.map( ( ws ) => ws?.location ) || [];
 
 process.env.FORCE_COLOR = '1';
 $.verbose = true;
 
-// dev task
 await Promise.all( [
 	$`node ./bin/vscode-helper.mjs --watch`,
 	$`npx browser-sync start --config bs-config.js`,
-	...workspaces.map( ( ws ) => $`npm run dev --if-present -w ${ ws }` ),
+	workspaces.map( ( ws ) => $`npm run dev --if-present -w ${ ws }` ),
 ] );
