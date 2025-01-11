3ae74a8e88ca0e2156e4fe5398b1230a6c519038
motake <munetoshi_otake@camp4.jp>
2025-01-11 13:58:33 +0900

    fix error catch

--- bin/lint.mjs
+++ bin/lint.mjs
@@ -1,26 +1,38 @@
+/* eslint-disable no-console */
+import { findWorkspaces } from 'find-workspaces';
+import { relative } from 'node:path';
 import { $ } from 'zx';
 
-const packageJson = await import( '../package.json', {
-	with: { type: 'json' },
-} );
-const { workspaces } = packageJson.default;
+const workspaces =
+	findWorkspaces()?.map( ( ws ) =>
+		relative( process.cwd(), ws?.location )
+	) || [];
 
-$.verbose = true;
-process.env.FORCE_COLOR = '1';
+const init = async () => {
+	$.verbose = true;
+	process.env.FORCE_COLOR = '1';
 
-// format
-if ( process.argv.includes( '--format' ) ) {
-	await $`npx wp-scripts format . '!source'`;
-	await $`npm run format --workspaces --if-present`;
-	workspaces.forEach( async ( ws ) => {
-		await $`docker run --rm --volume $PWD:/app composer composer format ${ ws }`;
-	} );
-}
+	/**
+	 * format
+	 */
+	if ( process.argv.includes( '--format' ) ) {
+		await $`npx wp-scripts format . '!source'`;
+		await $`npm run format --workspaces --if-present`;
+		for ( const ws of workspaces ) {
+			await $`docker run --rm --volume $PWD:/app composer composer format ${ ws }`;
+		}
+		return;
+	}
 
-// lint
-else {
+	/**
+	 * lint
+	 */
 	await $`npm run lint --workspaces --if-present`;
-	workspaces.forEach( async ( ws ) => {
+	for ( const ws of workspaces ) {
 		await $`docker run --rm --volume $PWD:/app composer composer lint ${ ws }`;
-	} );
-}
+	}
+};
+
+init().catch( ( error ) =>
+	console.error( error?._stderr || error?.stderr || error )
+);
