1752f4b0d42acdb21a87d1d735009ad3aa78b26e
motake <munetoshi_otake@camp4.jp>
2025-01-20 19:41:30 +0900

    simplification

--- source/wp-content/themes/mysite/postcss.config.js
+++ source/wp-content/themes/mysite/postcss.config.js
@@ -2,10 +2,7 @@
 module.exports = ( ctx ) => ( {
 	plugins: {
 		'postcss-easy-import': {},
-		'postcss-preset-env': {
-			stage: 2,
-			features: { 'nesting-rules': true },
-		},
+		'postcss-preset-env': { stage: 2, features: { 'nesting-rules': true } },
 		'postcss-url': { filter: '**/_inline/*', url: 'inline' },
 		...( ctx.mode === 'production' && { cssnano: { preset: 'default' } } ),
 	},
