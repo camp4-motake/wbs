c4f527276492bd02066936977aa97a1fbdb7a1d5
motake <munetoshi_otake@camp4.jp>
2025-11-25 10:15:59 +0900

    chore: Move image optimization plugins to only run in production builds.

--- themes/wbs/webpack.config.js
+++ themes/wbs/webpack.config.js
@@ -11,47 +11,6 @@ for ( const cfg of config ) {
 	cfg.stats = 'minimal';
 
 	cfg.plugins.push(
-		new ImageMinimizerPlugin( {
-			test: /\.(svg)$/i,
-			minimizer: {
-				implementation: ImageMinimizerPlugin.svgoMinify,
-				options: {
-					encodeOptions: {
-						multipass: true,
-						plugins: [ 'preset-default' ],
-					},
-				},
-			},
-		} ),
-
-		new ImageMinimizerPlugin( {
-			deleteOriginalAssets: false,
-			generator: [
-				...[
-					{ avif: { quality: 80, chromaSubsampling: '4:2:0' } },
-				].map( ( opt ) => ( {
-					type: 'asset',
-					implementation: ImageMinimizerPlugin.sharpGenerate,
-					filter: imgFilter,
-					options: { encodeOptions: opt },
-				} ) ),
-			],
-		} ),
-
-		new ImageMinimizerPlugin( {
-			minimizer: {
-				implementation: ImageMinimizerPlugin.sharpMinify,
-				options: {
-					encodeOptions: {
-						jpeg: { quality: 80 },
-						webp: { quality: 90, smartSubsample: true },
-						avif: { quality: 80 },
-						png: { quality: 80, palette: true, colors: 128 },
-					},
-				},
-			},
-		} ),
-
 		new CopyPlugin( {
 			patterns: [
 				{
@@ -63,6 +22,51 @@ for ( const cfg of config ) {
 			],
 		} )
 	);
+
+	if ( process.env.NODE_ENV === 'production' ) {
+		cfg.plugins.push(
+			new ImageMinimizerPlugin( {
+				test: /\.(svg)$/i,
+				minimizer: {
+					implementation: ImageMinimizerPlugin.svgoMinify,
+					options: {
+						encodeOptions: {
+							multipass: true,
+							plugins: [ 'preset-default' ],
+						},
+					},
+				},
+			} ),
+
+			new ImageMinimizerPlugin( {
+				deleteOriginalAssets: false,
+				generator: [
+					...[
+						{ avif: { quality: 80, chromaSubsampling: '4:2:0' } },
+					].map( ( opt ) => ( {
+						type: 'asset',
+						implementation: ImageMinimizerPlugin.sharpGenerate,
+						filter: imgFilter,
+						options: { encodeOptions: opt },
+					} ) ),
+				],
+			} ),
+
+			new ImageMinimizerPlugin( {
+				minimizer: {
+					implementation: ImageMinimizerPlugin.sharpMinify,
+					options: {
+						encodeOptions: {
+							jpeg: { quality: 80 },
+							webp: { quality: 90, smartSubsample: true },
+							avif: { quality: 80 },
+							png: { quality: 80, palette: true, colors: 128 },
+						},
+					},
+				},
+			} )
+		);
+	}
 
 	cfg.module.rules.push( {
 		test: /\.(jpe?g|png|gif|svg)$/i,
